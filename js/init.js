9fd258da1fedfc31bf31fe60a1880415dcf540cc
Li Chen <cxli@email.unc.edu>
2017-12-17 18:40:18 -0500

    updated

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
